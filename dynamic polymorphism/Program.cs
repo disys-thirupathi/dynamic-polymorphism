5657a41943bf324e593e713273ed4af3cf1bf4f8
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 18:23:08 +0530

    Add project files.

---
+++ dynamic polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DynamicPoly
+{
+    public class Interest
+    {
+        public virtual double MyBank(double amount, double rate)
+        {
+            return amount + (amount * rate);
+        }
+    }
+    public class SimpleInterest : Interest
+    {
+        public override double MyBank(double amount, double rate)
+        {
+            return amount + (amount * rate) + 2500;
+        }
+    }
+    public class ExpressInterest : Interest
+    {
+        public override double MyBank(double amount, double rate)
+        {
+            return amount + (amount * rate) + 5000;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Interest obj = new Interest();
+            double final = obj.MyBank(50000, 0.25);
+            Console.WriteLine("Normal interest " + final);
+
+            obj = new SimpleInterest();
+            final = obj.MyBank(50000, 0.5);
+            Console.WriteLine("Simple interest " + final);
+
+            obj = new ExpressInterest();
+            final = obj.MyBank(50000, 0.8);
+            Console.WriteLine("Express interest " + final);
+
+        }
+    }
+}
